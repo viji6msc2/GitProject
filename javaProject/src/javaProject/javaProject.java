9de2757c758347482a2907e165498e477d485ec5
mukesh214 <mukesh.kumar@agentmedium.com>
2018-08-29 19:13:46 +0530

    1203

---
+++ javaProject/src/javaProject/javaProject.java
@@ -0,0 +1,13 @@
+package javaProject;
+
+public class javaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Test");
+
+	}
+
+}
